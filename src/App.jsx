58042c7e88d92a4e7a4ee85897c906d787dd834d
Harsh Gupta <harshgupta82003@gmail.com>
2026-03-25 09:19:59 +0530

    feat : add employee card

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Employees from "./components/Employees";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
@@ -7,7 +8,8 @@ function App() {
     <>
       <div className="flex flex-col w-full min-h-screen">
         <Navbar />
-        <div className="flex-1">
+        <div className="flex-1 py-10">
+          <Employees/>
 
 
         </div>
